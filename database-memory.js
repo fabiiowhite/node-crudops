bac0909a01118f2d9561fa347c30dbbef96be3ac
Fabinho <fabio.o.white@gmail.com>
2024-02-23 19:18:43 +0200

    added functions for a local database

--- database-memory.js
+++ database-memory.js
@@ -1,24 +1,37 @@
+//UUID = Universal Unique ID
 import { randomUUID } from 'node:crypto'
 
 export class DatabaseMemory {
     #videos = new Map()
 
+    //Function to list all elements  in the database.
+    list() {
+        return Array.from(this.#videos.entries()).map((videoArray) => {
+            const id = videoArray[0]
+            const data = videoArray[1]
+
+            return {
+                id,
+                ...data,
+            }
+        })
+    }
+
+    //Function to add a new video in the database.
     create (video) {
-        //Universal Unique ID
         const videoId = randomUUID()
 
         this.#videos.set(videoId, video)
     }
 
+    //Function to update a video based on it's ID
     update(id, video){
         this.#videos.set(id, video)
     }
 
-    update(id){
+    //Function to get a specific video by it's ID.
+    delete(id){
         this.#videos.delete(id)
     }
 
-    list() {
-        return this.#videos.values()
-    }
 }
