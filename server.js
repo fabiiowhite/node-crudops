0dd7ec90d8b620662a7ece06e6c21ca81abeb492
Fabinho <fabio.o.white@gmail.com>
2024-02-23 19:19:50 +0200

    finished server code for local memory operations

--- server.js
+++ server.js
@@ -2,27 +2,61 @@ import { fastify } from 'fastify'
 import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
+const database = new DatabaseMemory()
 
-// GET, POST, PUT, DELETE, PATCH
+//Request Body
 
-server.post('/videos', () => {
-    return 'create videos'
+//Create a video
+server.post('/videos', (request,reply) => {
+    const { title, description, duration } = request.body
+    
+    database.create({
+        title,
+        description,
+        duration
+    })
+
+    console.log((database.list()))
+
+    return reply.status(201).send()
 })
 
+//Read or List all videos
 server.get('/videos', () => {
-    return 'read a video'
+    const videos = database.list()
+
+    console.log(videos)
+
+    return videos
 })
 
-// Route parameter, the parameter specifying the target (id)
+// Note: Route parameter, the parameter specifying the target (id)
 
-server.put('/videos/:id', () => {
-    return 'update video'
+//Edit videos based on their IDs
+server.put('/videos/:id', (request,reply) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+
 })
 
-server.delete('/videos/:id', () => {
-    return 'delete video'
+//Delete videos based only on their  ID's 
+server.delete('/videos/:id', (request,reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
+//Local host port
 server.listen({
     port:3333,
 })
